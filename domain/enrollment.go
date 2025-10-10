c4a36c4aa2828dff5b46e4cf4c4f719ed30521f5
Emmanuel Ortega <emmanuel.ortega@jikkosoft.com>
2025-10-09 22:11:02 -0500

    the path was ajusted

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -9,11 +9,11 @@ import (
 
 type Enrollment struct {
 	Id        string         `json:"id" gorm:"type:char(36);not null;primaryKey;uniqueIndex"`
-	UserId    string         `json:"user_id,omitempty" gorm:"type:char(36)"`
-	User      *User          `json:"user,omitempty"`
-	CourseId  string         `json:"course_id" gorm:"type:char(36);not null"`
-	Course    *Course        `json:"course,omitempty"`
-	Status    string         `json:"status" gorm:"type:char(2)"`
+	UserId    string  `json:"user_id,omitempty" gorm:"type:char(36)"`
+	User      *User   `json:"user,omitempty"`
+	CourseId  string  `json:"course_id" gorm:"type:char(36);not null"`
+	Course    *Course `json:"course,omitempty"`
+	Status    string  `json:"status" gorm:"type:char(2)"`
 	CreatedAt *time.Time     `json:"-"`
 	UpdatedAt *time.Time     `json:"-"`
 	DeletedAT gorm.DeletedAt `json:"-"`
